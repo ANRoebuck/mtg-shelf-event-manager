295210cd6149d07aea93e436fdb7ac49d76e7cb7
anroebuck <aroebuck@quantbet.com>
2021-07-13 22:25:54 +0100

    implement pairing algorithm

--- components/Round.js
+++ components/Round.js
@@ -1,4 +1,9 @@
-const { randomElementFromArray } = require("./utils");
+const { lowestFirst,
+  incrementObjectOfNumbers,
+  peek,
+  pushToObjectOfArrays,
+  randomElementFromArray
+} = require("./utils");
 const { Match } = require('./Match');
 const { Bye } = require('./Player');
 
@@ -14,22 +19,24 @@ class Round {
 
   getRoundNumber = () => this.roundNumber;
 
+  isComplete = () => this.getPairings().every(p => p.isComplete());
+
   getPlayers = () => this.players;
 
   getRankedPlayers = () => this.getPlayers().sort((a, b) => a.getPoints() - b.getPoints());
 
   getPairings = () => this.pairings;
 
+  createPairing = (player1, player2) => this.pairings.push(new Match(player1, player2));
+
   setUp = () => {
     let rankedPlayers = [...this.getRankedPlayers()];
-
-    console.log(rankedPlayers);
 
     if (rankedPlayers.length % 2 === 1) {
       rankedPlayers = this.assignBye(rankedPlayers);
     }
 
-    while(rankedPlayers.length > 0) {
+    while (rankedPlayers.length > 0) {
       let playerToPair = rankedPlayers.pop();
 
       let targetPoints = playerToPair.getPoints();
@@ -38,7 +45,7 @@ class Round {
         potentialOpponents = rankedPlayers
           .filter(player => player.getPoints() === targetPoints)
           .filter(player => !player.previousOpponentOf(playerToPair));
-        targetPoints -=1;
+        targetPoints -= 1;
       }
 
       if (potentialOpponents.length === 0) {
@@ -55,13 +62,56 @@ class Round {
 
   pairRound = () => {
 
-    // create graph of players
 
-    // eliminate any impossible pairings (already played each other)
+    let rankedPlayers = [...this.getRankedPlayers()];
+    const highestPoints = peek(rankedPlayers).getPoints();
 
-    // check if for any player, only one possible pairing remains
-    //    --> pair that
+    // if odd number of players
+    // assign bye and remove player from graph
+    //
+    if (rankedPlayers.length % 2 === 1) {
+      rankedPlayers = this.assignBye(rankedPlayers);
+    }
 
+    let candidatePairings = this.getPossiblePairings(rankedPlayers);
+
+    // while not all players are paired
+    while (candidatePairings.length > 0) {
+
+      let playerA;
+      let playerB;
+
+
+      // if for any player, only one possible pairing remains
+      //    --> pair that
+      //
+      let aPlayerWithOnlyOneViablePairing =
+        this.findPlayerWithOnlyOneValidPairing(this.getNumberOfPossibleParingsByPlayer(rankedPlayers));
+      if (aPlayerWithOnlyOneViablePairing) {
+        let [a, b] = candidatePairings.filter(cp => cp.contains(aPlayerWithOnlyOneViablePairing));
+        playerA = a;
+        playerB = b;
+      }
+
+      // else weight each pairing
+      // of the highest weighted pairings, pair one at random
+      else {
+        const candidatePairingsByWeight = candidatePairings.reduce((tally, pairing) =>
+          pushToObjectOfArrays(tally, this.getWeightForPairing(highestPoints, pairing), pairing), {})
+
+        const highestWeightedPairings = Object.entries(candidatePairingsByWeight).sort(lowestFirst)[0];
+        let [a, b]  = randomElementFromArray(highestWeightedPairings);
+        playerA = a;
+        playerB = b;
+      }
+
+
+      // finally
+      // remove paired players from graph
+      candidatePairings = candidatePairings.filter(cp => !cp.contains(playerA) && !cp.contains(playerB));
+    }
+
+    // all players should now be paired
   }
 
   assignBye = (rankedPlayers) => {
@@ -76,10 +126,69 @@ class Round {
     return rankedPlayers;
   }
 
-  createPairing = (player1, player2) => this.pairings.push(new Match(player1, player2));
 
-  isComplete = () => this.getPairings().every(p => p.isComplete());
+  // returns an array of two-element arrays of unique pairings of players
+  // ie if [a, b] then no [b, a]
+  getPossiblePairings = () => {
+    let players = [...this.getPlayers()];
+
+    let possiblePairings = [];
+
+    while (players.length > 0) {
+      let player = players.pop();
+      players.forEach(opponent => possiblePairings.push([player, opponent]));
+    }
+
+    return possiblePairings.filter(([player, opponent]) => !opponent.previousOpponentOf(player));
+  }
+
+
+  // returns an object of key:number pairs
+  // key is a player
+  // value is the number of pairings in which that player appears
+  getNumberOfPossibleParingsByPlayer = (candidatePairings) =>
+    candidatePairings.reduce((tally, pairedPlayers) => {
+      pairedPlayers.forEach(p => incrementObjectOfNumbers(tally, p));
+      return tally;
+    }, {});
+
+
+  // accepts an object of key:number pairs
+  // returns the first key whose value is exactly 1, or else null
+  findPlayerWithOnlyOneValidPairing = (pairingsByPlayer) => {
+    let player = null;
+    Object.entries(pairingsByPlayer).some(([k, v]) => {
+      if (v === 1) {
+        player = k;
+        return true;
+      }
+      return false;
+    });
+    return player;
+  }
+
+
+  // accepts a candidate pairing as a two-element array
+  // returns a number
+  getWeightForPairing = (highestPoints, [player, opponent]) => {
+
+    const best = Math.max(player.getPoints(), opponent.getPoints());
+    const worst = Math.min(player.getPoints(), opponent.getPoints());
+    const spread = best - worst;
+
+    // closeness is an inverse measure of how far the lower scoring player is behind the higher scoring player
+    const closeness = highestPoints - spread;
+
+    // weighting by closeness means pair-downs will be minimised
+    // weighting by the higher player's score means lower ranked players are more likely to be paired down than higher ranked players
+
+    // best and closeness are incremented by 1 to prevent multiplication by 0
+    return ( (best + 1) ** 2 ) * ( ( closeness + 1 ) ** 2 );
+  }
+  // https://bluebones.net/2018/04/swiss-pairing-algorithm/
+
 }
+
 
 module.exports = {
   Round,
--- components/utils.js
+++ components/utils.js
@@ -2,6 +2,21 @@
 
 const randomElementFromArray = (arr) => arr[Math.floor(Math.random()*arr.length)];
 
+const peek = (arr) => arr[arr.length - 1];
+
+const lowestFirst = (a,b) => a - b;
+
+const incrementObjectOfNumbers = (object, key) => {
+  if (!object[key]) object[key] = 1;
+  else object[key] += 1;
+  return object;
+}
+const pushToObjectOfArrays = (object, key, value) => {
+  if (!object[key]) object[key] = [value];
+  else object[key].push(value);
+  return object;
+}
+
 
 
 
