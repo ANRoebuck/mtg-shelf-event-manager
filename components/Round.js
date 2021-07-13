4363b784a34396b4398d6a6096c3b4c9f7a8f0ac
anroebuck <aroebuck@quantbet.com>
2021-07-13 22:26:34 +0100

    remove defunct method

--- components/Round.js
+++ components/Round.js
@@ -14,7 +14,7 @@ class Round {
     this.roundNumber = roundNumber;
     this.players = players;
     this.pairings = [];
-    this.setUp();
+    this.pairRound();
   }
 
   getRoundNumber = () => this.roundNumber;
@@ -29,39 +29,9 @@ class Round {
 
   createPairing = (player1, player2) => this.pairings.push(new Match(player1, player2));
 
-  setUp = () => {
-    let rankedPlayers = [...this.getRankedPlayers()];
 
-    if (rankedPlayers.length % 2 === 1) {
-      rankedPlayers = this.assignBye(rankedPlayers);
-    }
-
-    while (rankedPlayers.length > 0) {
-      let playerToPair = rankedPlayers.pop();
-
-      let targetPoints = playerToPair.getPoints();
-      let potentialOpponents = [];
-      while (potentialOpponents.length === 0 && targetPoints > -1) {
-        potentialOpponents = rankedPlayers
-          .filter(player => player.getPoints() === targetPoints)
-          .filter(player => !player.previousOpponentOf(playerToPair));
-        targetPoints -= 1;
-      }
-
-      if (potentialOpponents.length === 0) {
-        // failed to find a valid opponent, this is very bad
-      }
-
-      const opponent = randomElementFromArray(potentialOpponents);
-      this.createPairing(playerToPair, opponent);
-
-      rankedPlayers = rankedPlayers.filter(player => player.getPlayerId() !== opponent.getPlayerId());
-    }
-
-  }
-
+  // THE BUSINESS
   pairRound = () => {
-
 
     let rankedPlayers = [...this.getRankedPlayers()];
     const highestPoints = peek(rankedPlayers).getPoints();
